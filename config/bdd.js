49826226b38dca3f905252381d498b722bb37222
Marion <bozzomarion59@gmail.com>
2025-12-19 11:37:54 +0100

    Dossier Back

---
+++ config/bdd.js
@@ -0,0 +1,20 @@
+import mysql2 from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const bdd = mysql2.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+bdd.getConnection()
+    .then(() => 
+        console.log("database OK 🟢"))
+    .catch((err) => {
+        console.error("database connection error 🔴:", err);
+    });
+
+export default bdd;
